06c4897803c5dabab2dd9aae76eb813971c5eff3
Enrique <enriquedlc2d@gmail.com>
2023-06-08 14:39:48 +0100

    Feature: when the list is empty, it shows the error

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,7 +3,13 @@ import java.util.List;
 public class FizzBuzz {
 
     public List<String> fizzBuzz(List<Integer> fizzBuzzList) {
-        throw new Error("not implemented yet");
+        try {
+            if (fizzBuzzList.isEmpty()) throw new Exception("Empty list");
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        return null;
     }
 
 }
